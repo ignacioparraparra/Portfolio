1f875e1e2d1147f58b919a9fd1572cfa41542c26
Ignacio Parra Parra <Ignacioparra57@gmail.com>
2025-09-10 08:35:38 -0700

    plugin changes

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import netlify from "@netlify/vite-plugin";
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
 
-  plugins: [netlify()],
+  plugins: [netlify(), react()]
+
 })
