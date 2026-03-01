628e4e0238ec6fc96202ea950f52da21453c0d2a
Ignacio Parra <ignacioparra57@gmail.com>
2026-02-28 19:28:27 -0800

    fixed bars from bumbing div

--- my-app/app/src/Slider.tsx
+++ my-app/app/src/Slider.tsx
@@ -5,19 +5,27 @@ function Slider() {
             <div className='marquee'>
                 <div className='marquee-track'>
                     <div className='marquee-group'>
-                       <span>WELCOME TO MY WEBSITE MY NAME IS IGNACIO</span>
-                       <span>FAVORITE LANGUAGES INCLUDE C++ PYTHON JAVASCRIPT</span>
-                       <span>I LOVE HIKING ROCK CLIMBING AND DOGS</span>
-                       <span>WELCOME TO MY WEBSITE MY NAME IS IGNACIO</span>
-                       <span>FAVORITE LANGUAGES INCLUDE C++ PYTHON JAVASCRIPT</span>
-                       <span>I LOVE HIKING ROCK CLIMBING AND DOGS</span>
-                   </div>
-                    <div className='marquee-group'>
                         <span>I LOVE HIKING ROCK CLIMBING AND DOGS</span>
-                        <span>WELCOME TO MY WEBSITE MY NAME IS IGNACIO</span>
+                        <span>WELCOME TO MY WEBSITE</span>
                         <span>FAVORITE LANGUAGES INCLUDE C++ PYTHON JAVASCRIPT</span>
                         <span>I LOVE HIKING ROCK CLIMBING AND DOGS</span>
-                        <span>WELCOME TO MY WEBSITE MY NAME IS IGNACIO</span>
+                        <span>WELCOME TO MY WEBSITE</span>
+                        <span>FAVORITE LANGUAGES INCLUDE C++ PYTHON JAVASCRIPT</span>
+                    </div>
+                    <div className='marquee-group'>
+                        <span>I LOVE HIKING ROCK CLIMBING AND DOGS</span>
+                        <span>WELCOME TO MY WEBSITE</span>
+                        <span>FAVORITE LANGUAGES INCLUDE C++ PYTHON JAVASCRIPT</span>
+                        <span>I LOVE HIKING ROCK CLIMBING AND DOGS</span>
+                        <span>WELCOME TO MY WEBSITE</span>
+                        <span>FAVORITE LANGUAGES INCLUDE C++ PYTHON JAVASCRIPT</span>
+                    </div>
+                    <div className='marquee-group'>
+                        <span>I LOVE HIKING ROCK CLIMBING AND DOGS</span>
+                        <span>WELCOME TO MY WEBSITE</span>
+                        <span>FAVORITE LANGUAGES INCLUDE C++ PYTHON JAVASCRIPT</span>
+                        <span>I LOVE HIKING ROCK CLIMBING AND DOGS</span>
+                        <span>WELCOME TO MY WEBSITE</span>
                         <span>FAVORITE LANGUAGES INCLUDE C++ PYTHON JAVASCRIPT</span>
                     </div>
                 </div>
