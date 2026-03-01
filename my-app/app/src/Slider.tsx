962ee41ac7d598ceec714a71d51d36ac6392e368
Ignacio Parra <ignacioparra57@gmail.com>
2026-02-28 17:11:19 -0800

    added favicon and fixed bar jumps

--- my-app/app/src/Slider.tsx
+++ my-app/app/src/Slider.tsx
@@ -13,12 +13,12 @@ function Slider() {
                        <span>I LOVE HIKING ROCK CLIMBING AND DOGS</span>
                    </div>
                     <div className='marquee-group'>
-                       <span>WELCOME TO MY WEBSITE MY NAME IS IGNACIO</span>
-                       <span>FAVORITE LANGUAGES INCLUDE C++ PYTHON JAVASCRIPT</span>
-                       <span>I LOVE HIKING ROCK CLIMBING AND DOGS</span>
-                       <span>WELCOME TO MY WEBSITE MY NAME IS IGNACIO</span>
-                       <span>FAVORITE LANGUAGES INCLUDE C++ PYTHON JAVASCRIPT</span>
-                       <span>I LOVE HIKING ROCK CLIMBING AND DOGS</span>
+                        <span>I LOVE HIKING ROCK CLIMBING AND DOGS</span>
+                        <span>WELCOME TO MY WEBSITE MY NAME IS IGNACIO</span>
+                        <span>FAVORITE LANGUAGES INCLUDE C++ PYTHON JAVASCRIPT</span>
+                        <span>I LOVE HIKING ROCK CLIMBING AND DOGS</span>
+                        <span>WELCOME TO MY WEBSITE MY NAME IS IGNACIO</span>
+                        <span>FAVORITE LANGUAGES INCLUDE C++ PYTHON JAVASCRIPT</span>
                     </div>
                 </div>
             </div>
